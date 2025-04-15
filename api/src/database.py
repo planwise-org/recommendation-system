e782cf107151ceeccab59c80e3ea40d03f623c22
jose-izarra <jaiza0912@gmail.com>
2025-04-15 16:13:23 +0300

    added testing workflow

--- api/src/database.py
+++ api/src/database.py
@@ -6,8 +6,13 @@ from supabase import create_client, Client
 
 # when on production, use the supabase client
 # when on local, use the local database connection created on the docker-compose file
+# when on test, use in-memory SQLite database
 
-if os.environ.get("ENV") == "local":
+if os.environ.get("ENV") == "test":
+    DATABASE_URL = "sqlite:///:memory:"
+    engine = create_engine(DATABASE_URL)
+
+elif os.environ.get("ENV") == "local":
     # Creates a connection to the database according to the env variables
     DBUSER = os.environ.get("DBUSER")
     DBPASS = os.environ.get("DBPASS")
@@ -17,18 +22,18 @@ if os.environ.get("ENV") == "local":
     DATABASE_URL = f"postgresql://{DBUSER}:{DBPASS}@{DBHOST}:{DBPORT}/{DBNAME}"
     engine = create_engine(DATABASE_URL)
 
+
 elif os.environ.get("ENV") == "prod":
     url: str = os.environ.get("SUPABASE_URL")
     key: str = os.environ.get("SUPABASE_KEY")
     supabase: Client = create_client(url, key)
 
-
 def init_db():
-    if os.environ.get("ENV") == "local":
+    if os.environ.get("ENV") in ["local", "test"]:
         SQLModel.metadata.create_all(engine)
 
 def get_db():
-    if os.environ.get("ENV") == "local":
+    if os.environ.get("ENV") in ["test", "local"]:
         with Session(engine) as session:
             yield session
     elif os.environ.get("ENV") == "prod":
